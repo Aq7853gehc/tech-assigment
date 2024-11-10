6e96299c7c0610afaf28561c44a3e5dd5f42e175
Aq7853gehc <aquib7583@gmail.com>
2024-11-10 09:54:32 +0530

    make responsive

--- components/Acttions.tsx
+++ components/Acttions.tsx
@@ -19,15 +19,22 @@ const Actions = () => {
       state: "West Bengal",
     },
     {
-      src: "/images/travel/collect/Frame3.png",
-      title: "Darjeeling",
-      state: "West Bengal",
+      src: "/images/travel/collect/Frame1.png",
+      title: "Delhi",
+      state: "Delhi",
+    },
+    {
+      src: "/images/travel/collect/Frame1.png",
+      title: "Delhi",
+      state: "Delhi",
+    },
+    {
+      src: "/images/travel/collect/Frame2.png",
+      title: "Munnar",
+      state: "Keral",
     },
   ];
 
-  const list= [
-    {id:}
-  ]
   return (
     <main className="w-full flex flex-col mb-4 gap-6">
       <section className="w-full flex justify-between items-center rounded-3xl h-28  bg-black px-4">
@@ -54,13 +61,25 @@ const Actions = () => {
           <h3 className="text-[2.125rem] text-center ">
             Explore Our Curated Travel Itineraries
           </h3>
-          <div className="flex flex-row gap-6">
+          <div className="flex overflow-x-auto w-full space-x-10 py-4 px-5 scrollbar-hide">
             {image.map((img, idx) => (
-              <div className="relative" key={idx}>
-                <Image src={img.src} alt="image" width={270} height={480} />
+              <div
+                className="relative flex-shrink-0"
+                key={idx}
+                style={{ width: 270 }}
+              >
+                <Image
+                  src={img.src}
+                  alt="image"
+                  width={270}
+                  height={480}
+                  className="rounded-lg"
+                />
                 <div className="absolute bottom-2 gap-0 flex flex-col justify-center items-center w-full">
-                  <h3 className="font-bold text-2xl">{img.title} Itenrary</h3>
-                  <p className="font-bold text-lg">{img.state}</p>
+                  <h3 className="font-bold text-2xl text-white">
+                    {img.title} Itinerary
+                  </h3>
+                  <p className="font-bold text-lg text-white">{img.state}</p>
                   <div className="flex gap-2 px-3 py-1 rounded-full bg-white">
                     <p className="text-lg font-bold text-black">Samira Hadid</p>
                     <Image
@@ -91,22 +110,40 @@ const Actions = () => {
             className="w-full object-cover"
           />
 
-          
-            <svg className="absolute left-[10rem]  inset-0 w-full h-full">
-              <path
-                d="M 0,0 A 150,100 0 0,0 1200,0"
-                fill="none"
-                stroke="white"
-                strokeWidth="2"
-                strokeDasharray="10, 10"
-              />
-            </svg>
-            <div className="absolute w-full left-60 top-52 h-full">
-              <div className="max-w-sm w-full">
-                
+          <svg className="absolute left-[10rem]  inset-0 w-full h-full">
+            <path
+              d="M 0,0 A 150,100 0 0,0 1200,0"
+              fill="none"
+              stroke="white"
+              strokeWidth="2"
+              strokeDasharray="10, 10"
+            />
+          </svg>
+          <div className="absolute w-full  top-[14.5rem] h-full">
+            <div className="max-w-[67rem] mx-auto w-full flex justify-between ">
+              <div className="flex flex-col gap-4  items-center">
+                <FaPlane className={`w-10 h-10 rotate-45`} />
+                <h1 className="text-xl font-bold ">1</h1>
+                <p className="font-bold text-[1.6rem] max-w-[10rem] w-full text-center">
+                  Create Itinerary
+                </p>
+              </div>
+              <div className="flex flex-col gap-4  relative top-[9.2rem]  items-center">
+                <FaPlane className={`w-10 h-10 rotate-0`} />
+                <h1 className="text-xl font-bold ">2</h1>
+                <p className="font-bold text-[1.6rem] max-w-[13rem] w-full text-center">
+                  Convert into Travel Package
+                </p>
+              </div>
+              <div className="flex relative left-5 gap-4 flex-col items-center">
+                <FaPlane className={`w-10 h-10 -rotate-45`} />
+                <h1 className="text-xl font-bold ">3</h1>
+                <p className="font-bold text-[1.6rem] max-w-[10rem] w-full text-center">
+                  Earn by sharing
+                </p>
               </div>
             </div>
-           
+          </div>
         </section>
       </div>
     </main>
