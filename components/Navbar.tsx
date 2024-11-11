a0985629d0632faa5a80b5fdde3490a388939fbf
Aq7853gehc <aquib7583@gmail.com>
2024-11-11 21:24:52 +0530

    fix the error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import { BiMenu } from "react-icons/bi";
 import { FiChevronDown, FiSearch, FiUser } from "react-icons/fi";
 import ProfileDropdown from "./Profiledropdown";
 
