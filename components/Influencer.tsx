f651f68f7efa9fa50b323e34384d93a105997ac5
Aq7853gehc <aquib7583@gmail.com>
2024-11-11 12:14:17 +0530

    Influnecer work done

--- components/Influencer.tsx
+++ components/Influencer.tsx
@@ -1,21 +1,41 @@
-import Image from "next/image"
+import Image from "next/image";
+import { MdArrowOutward } from "react-icons/md";
 
-const Influencer = ()=>{
-    const pics = [
-        "/images/infulencer/1.png",
-        "/images/infulencer/2.png",
-        "/images/infulencer/3.png",
+const Influencer = () => {
+  const pics = [
+    { src: "/images/infulencer/1.png", title: "Samira" },
+    { src: "/images/infulencer/2.png", title: "Samira" },
+    { src: "/images/infulencer/3.png", title: "Jay" },
+    { src: "/images/infulencer/4.png", title: "Jay" },
+  ];
+  return (
+    <main className=" relative flex gap-10 flex-col w-full">
+      <h1 className="text-[#00dc82] text-4xl font-bold text-center">
+        Our Influencers
+      </h1>
+      <div className="size-64 rounded-full grid grid-cols-4 gap-10 w-full">
+        {pics.map((itms, idx) => (
+          <div key={idx} className="relative grayscale hover:grayscale-0 group">
+            <div className="absolute top-3 left-2 w-full h-full rounded-full bg-gradient-to-r from-pink-500 to-blue-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300  " />
 
-    ]
-    return (
-        <main className="flex flex-col w-full justify-between">
-            <h1 className="text-[#00dc82] text-4xl font-bold text-center">Our Influencers</h1>
-            <div className="size-64 rounded-full grayscale hover:grayscale-0">
-            <div className="bg-gradient-to-r absolute opacity-0 hover:opacity-100 left-2 z-[-1] top-3 rounded-full from-[#FF5D5D] to-[#4794ff] w-full h-full"/>
-            <Image src={pics[0]} alt="pics" width={268} height={268} className="object-contain rounded-full drop-shadow-md z-[10]"/>
+            <div className="flex gap-4 relative">
+              <Image
+                src={itms.src}
+                alt="pics"
+                width={256}
+                height={256}
+                className="rounded-full w-full h-full object-contain"
+              />
+              <div className="px-2 flex gap-2 items-center justify-center absolute top-[60%] left-[40%] bg-white rounded-xl opacity-0 group-hover:opacity-100">
+                <h1 className="text-2xl text-black font-bold">{itms.title}</h1>
+                <MdArrowOutward className="text-black w-6 h-6" />
+              </div>
             </div>
-        </main>
-    )
-}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
 
-export default Influencer
+export default Influencer;
